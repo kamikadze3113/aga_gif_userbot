d150fd310c53be59b886c2a37eb7ab88a38331cd
kamikadze3113 <68239296+kamikadze3113@users.noreply.github.com>
2020-07-19 15:39:39 +0300

    Update

--- aga.py
+++ aga.py
@@ -1,3 +1,6 @@
+#create kamikadze3113
+#thanks @dashaushakova
+#go to the https://t.me/friendlytgbot_ru / @friendlytgbot_ru
 import asyncio
 from .. import loader
 import random
@@ -9,6 +12,7 @@ class АгаMod(loader.Module):
 	strings = {"name": "Ага"}
 	async def агаcmd(self, message):
 		await message.edit("Ищу...")
-		time.sleep(3)
+		time.sleep(1)
+		await message.delete()
 		GIF = ["https://i.gifer.com/73H4.gif", "https://i.gifer.com/TfGF.gif", "https://i.gifer.com/Okak.gif", "https://i.gifer.com/7DSA.gif"]
 		await message.client.send_file(message.to_id, random.choice(GIF))
